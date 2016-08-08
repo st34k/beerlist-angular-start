bbbf945475b501b2b1f0346c9f390e49e594f05e
Sashko <Sashko>
2016-08-08 18:27:53 +0300

    finished expansion assingments. need to prettify the css

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,13 +1,36 @@
 app.controller('MainCtrl', function($scope, beerService){
   $scope.allBeers = beerService.beers;
+
+
   $scope.addBeer = function(){
-
     beerService.addBeer($scope.name, $scope.style, $scope.abv, $scope.image)
-
+    $scope.name="";
+    $scope.style="";
+    $scope.abv="";
+    $scope.image="";
   }
 
   $scope.removeBeer = function(index){
     beerService.removeBeer(index)
   }
+
+  $scope.getRating = function(beer,index){
+    if (beer === undefined){
+      return;
+    }
+    else{
+    beerService.rateBeer(beer,index);}
+
+  }
+
+  $scope.enableEdit = function(index){
+    beerService.toggleDeath(index);
+  }
+
+  $scope.submitEdits = function(editName, editStyle, index){
+
+    beerService.editBeers(editName, editStyle, index);
+  }
+
 
 })
--- js/services/beers.js
+++ js/services/beers.js
@@ -1,10 +1,10 @@
 app.service('beerService', function(){
 
   var beers = [
-    {name: 'Guiness', style: 'Dark', abv: '9%', imageUrl: 'img/guiness.jpg'},
-    {name: 'Tuborg', style: 'Dark', abv: '9%', imageUrl: 'img/tuborg.jpg'},
-    {name: 'Stella', style: 'Dark', abv: '9%', imageUrl: 'img/stella.jpg'},
-    {name: 'Kozel', style: 'Dark', abv: '9%', imageUrl: 'img/kozel.jpg'}
+    {name: 'Guiness', style: 'Dark', abv: '9%', imageUrl: 'img/guiness.jpg', rating:[4,5,5,3], avgRating: 0, edit:false},
+    {name: 'Tuborg', style: 'Dark', abv: '9%', imageUrl: 'img/tuborg.jpg', rating:[1,2,3,4,5], avgRating: 0, edit:false},
+    {name: 'Stella', style: 'Dark', abv: '9%', imageUrl: 'img/stella.jpg', rating:[2,2,4,5], avgRating: 0, edit:false},
+    {name: 'Kozel', style: 'Dark', abv: '9%', imageUrl: 'img/kozel.jpg', rating:[5,5,4,4], avgRating: 0, edit:false}
   ];
 
 
@@ -13,7 +13,9 @@ app.service('beerService', function(){
       name: name,
       style: style,
       abv: abv,
-      imageUrl: imageUrl
+      imageUrl: imageUrl,
+      rating: []
+      
     }
     beers.push(newBeer);
   }
@@ -22,6 +24,29 @@ app.service('beerService', function(){
     beers.splice(index,1);
   }
 
-return {addBeer: addBeer, beers: beers, removeBeer: removeBeer}
+  function rateBeer(rate,index){
+    var average = 0;
+    beers[index].rating.push(parseInt(rate));
+    for (var i = 0; i < beers[index].rating.length ; i++){
+
+      average += beers[index].rating[i];
+
+    }
+    average = average / beers[index].rating.length;
+    beers[index].avgRating = parseInt(average);
+  }
+
+  function toggleDeath(index){
+    beers[index].edit = !beers[index].edit;
+
+  }
+
+  function editBeers(name,style,index){
+    beers[index].name = name;
+    beers[index].style = style;
+    toggleDeath(index);
+  }
+
+return {addBeer: addBeer, beers: beers, removeBeer: removeBeer, rateBeer:rateBeer, toggleDeath:toggleDeath, editBeers: editBeers}
 
 })
